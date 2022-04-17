ab2399354a9e4b33edcf17dd9e4d96353331fd5d
hosglial <hosglial@gmail.com>
2022-04-17 15:04:59 +0300

    Refractoring

--- voltmeter/voltmeter.ino
+++ voltmeter/voltmeter.ino
@@ -1,12 +1,11 @@
 #include <String.h>
 #include <Wire.h> // библиотека для протокола I2C
 #include <LiquidCrystal_I2C.h> // библиотека для LCD 1602
-LiquidCrystal_I2C LCD(0x27,20,4); // присваиваем имя дисплею
 #include <SPI.h>                // Подключаем библиотеку SPI
 #include <SD.h>                 // Подключаем библиотеку SD
 #include <RTClib.h>
 #include <Adafruit_ADS1X15.h>
-Adafruit_ADS1115 ads;
+
 
 #define EN 2
 #define S0 3
@@ -15,39 +14,35 @@ Adafruit_ADS1115 ads;
 #define S3 6
 
 
-
-uint8_t pinArr[10] = {A1, A2, A3, A4, A5, A6, A7, A8, A9, A10}; //массив пинов
-float COEF = 0.0048; //коэффициент для перевода в вольты
-float COEFGALV = 1.77; //коэффициент для перевода в вольты
-Sd2Card card;                   // Указываем переменные
-SdVolume volume;                // Указываем переменные
-SdFile root;                    // Указываем переменные
+Adafruit_ADS1115 ads;			  // Объект для работы с АЦП
+LiquidCrystal_I2C LCD(0x27,20,4); // Объект для работы с дисплеем
+Sd2Card card;                     // Указываем переменные SD
+SdVolume volume;                  // Указываем переменные SD
+SdFile root;                      // Указываем переменные SD
  
 const int chipSelect = 49;      //адрес sd карты
 
-
-void SdSetup();
-
 RTC_DS3231 rtc;
+
+int controlPin[] = {S0, S1, S2, S3};
+
 
 String print_time(DateTime timestamp) {
   char message[120];
 
-  int Year = timestamp.year();
-  int Month = timestamp.month();
-  int Day = timestamp.day();
-  int Hour = timestamp.hour();
-  int Minute = timestamp.minute();
-  int Second= timestamp.second();
-
-  sprintf(message, "%d-%d-%d %02d:%02d:%02d", Month,Day,Year,Hour,Minute,Second);
+  sprintf(message, "%d-%d-%d %02d:%02d:%02d", 
+		  timestamp.month(),
+		  timestamp.day(),
+		  timestamp.year(),
+		  timestamp.hour(),
+		  timestamp.minute(),
+		  timestamp.second()
+  );
   
   return message;
 }
 
 int changeMux(int channel){
-
-  int controlPin[] = {S0, S1, S2, S3};
 
   int muxChannel[16][4]={
     {0,0,0,0}, //channel 0
@@ -77,21 +72,18 @@ int changeMux(int channel){
 void setup() {  
     Serial.begin(9600);
 
+	// Включение мультиплексора
     pinMode(EN, OUTPUT); 
     digitalWrite(EN, LOW);
-
-    pinMode(S0, OUTPUT); 
-    pinMode(S1, OUTPUT); 
-    pinMode(S2, OUTPUT); 
-    pinMode(S3, OUTPUT);   
-  
-    digitalWrite(S0, LOW);
-    digitalWrite(S1, LOW);
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, LOW);
+	
+	// Установка digital портов на выход и запись LOW сигнала на них
+	for(int i = 0; i < 4; i ++){
+		pinMode(controlPin[i], OUTPUT);
+		digitalWrite(controlPin[i], LOW);		
+	}
 
 
-  // Установка коэффициента усиления
+  // Установка коэффициента усиления 2/3
   ads.setGain(GAIN_TWOTHIRDS);
 
   // ВОЗМОЖНЫЕ ВАРИАНТЫ УСТАНОВКИ КУ:
@@ -102,24 +94,20 @@ void setup() {
   // ads.setGain(GAIN_EIGHT);     | 8х   | +/-0.512V | 1bit = 0.015625mV  |
   // ads.setGain(GAIN_SIXTEEN);   | 16х  | +/-0.256V | 1bit = 0.0078125mV |
 
-  ads.begin(0x4B); // Инициализация модуля ADS1115
-
-
+  if (!ads.begin(0x4B)) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
   
-  rtc.begin();
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+ 
+  rtc.begin(); // включение модуля реального времени
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));// настройка времени
 
   LCD.init(); // инициализация дисплея
   LCD.backlight(); // включение подсветки
     
   pinMode(22, OUTPUT);
   digitalWrite(22, LOW);
-
-  //настройка пинов на чтение (возможно нахуй не нужна будет)
-  for (int i = 0; i < 10; i++) {
-    pinMode(pinArr[i], INPUT);
-    analogWrite(pinArr[i], LOW);
-  }
 
    Serial.println("Initializing SD card...");
 
@@ -143,7 +131,6 @@ void loop() {
   DateTime n = rtc.now();
    
   String logData = String(print_time(n));
-//  String logData = String(rtc.now().toString(date)) + "    ";
   LCD.print(n.toString(date));
   
    for (int i = 0; i<10; i ++) {
@@ -157,31 +144,18 @@ void loop() {
    }
 
    Serial.println(logData);    
- 
-//  for (int i = 0; i<10; i++) {
-//    dataArr[i] = 0;
-//    dataArr[i] = analogRead(pinArr[i]);
-//    dataArr[i] *= COEF;
-//    dataArr[i] *= COEFGALV;
-//    //Serial.println("Voltage: " + String(i+1) + "-" + String(dataArr[i]));
-//    logData += String(dataArr[i]);
-//    logData += "    ";
-//    Serial.println(logData);    
-//    LCD.setCursor((i%4) * 5,int(i/4)+1); // ставим курсор на 1 символ первой строке
-//    LCD.print(dataArr[i]); // выводим напряжение на дисплей
-//    
-//  }
 
+  
 
 // Этот кусок пишет на карту, для тестов нахуй не нужен
-//  File dataFile = SD.open("log.txt", FILE_WRITE);
-//    if (dataFile) {
-//    dataFile.println(logData);
-//    dataFile.close();
-//  }
-//  else {
-//    Serial.println("error opening log.txt");
-//  }
+  File dataFile = SD.open("log.txt", FILE_WRITE);
+    if (dataFile) {
+    dataFile.println(logData);
+    dataFile.close();
+  }
+  else {
+    Serial.println("error opening log.txt");
+  }
   delay(500);
   LCD.clear(); // очищаем экран дисплея
 
